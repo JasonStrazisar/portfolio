cc24420c163931e6169c62eae01776b73eba6392
Jason STRAZISAR <jason.strazisar67@gmail.com>
2022-06-02 14:45:12 +0200

    feat: 🎸 create Hero Component for the homepage

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,22 +1,13 @@
 import React from "react";
 import styles from "./hero.module.css";
-import Image from "next/image";
 
 const Hero = () => (
-  <div>
-    <h1 className={styles.hero__title}>Jason Strazisar</h1>
-    <p className={styles.hero__subtitle}>
-      I&apos;m a{" "}
-      <span>
-        <Image width="30px" height="26px" src="/react.png" />
-      </span>{" "}
-      developer. Spending my time at{" "}
-      <span id="dvore">
-        <Image width="30px" height="43px" src="/dvore.png" />
-      </span>
-      {"  "}
-      helping building the best BI tool for restaurants.
-    </p>
+  <div className={styles.hero}>
+    <p className={styles.hero__subtitle}>Hey! I'm Jason 👋</p>
+    <h1 className={styles.hero__title}>
+      I’m a <span>Frontend</span> developer that have fun playing around with{" "}
+      <span>React</span>.
+    </h1>
   </div>
 );
 
