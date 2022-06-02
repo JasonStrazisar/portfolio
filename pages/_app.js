ff5a457173a432e53dc1f68b441ecb47643c51e8
Jason STRAZISAR <jason.strazisar67@gmail.com>
2022-06-02 14:44:29 +0200

    feat: 🎸 Add initial styling theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "../style/globals.css";
+import "../style/main.css";
+import "../style/theme.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,35 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+function Document() {
+  async function getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  const meta = {
+    title: "Jason's Portfolio",
+    description: "A portfolio of Jason's work",
+    image: "",
+  };
+
   return (
-    <Html>
+    <Html lang="en">
       <Head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
@@ -22,3 +40,5 @@ export default function Document() {
     </Html>
   );
 }
+
+export default Document;
